bd611a0fef7c4099f7f960f6a52f2141540200f4
Cedric Boucher <cedricjboucher@pm.me>
2025-06-24 10:23:50 -0600

    fix border colour for images

--- McDonalds_images.py
+++ McDonalds_images.py
@@ -15,7 +15,7 @@ if __name__ == "__main__":
     image_paths: list[str] = [
         r""
     ]
-    
+
     image_objects: list[Image.Image] = McDonalds.load_images(image_paths)
 
     img: Image.Image = Image.new("RGB", (McDonalds.IMG_SIZE, McDonalds.IMG_SIZE), "white")
@@ -23,7 +23,7 @@ if __name__ == "__main__":
 
     McDonalds.draw_center_letter(draw, McDonalds.FONT_SIZE_SCALING_FACTOR)
     McDonalds.draw_image_rings(img, radii, font_sizes, image_objects)
-    McDonalds.add_border(draw, McDonalds.DPI, 1.0, 127)
+    McDonalds.add_border(draw, McDonalds.DPI, 1.0, (127, 127, 127))
 
     if McDonalds.SAVE_AS_PDF:
         img.save("mcdonald_eye_chart_images.pdf")
